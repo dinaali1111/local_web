264b11e830bb09203cd83812fb138cef5953002b
dinaali1111 <dinaali97789@gmail.com>
2025-08-24 06:53:57 +0200

    edit path

--- app/admin/all-places/page.jsx
+++ app/admin/all-places/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import axios from "axios";
+// ...existing code...
 import {
   Box,
   Typography,
